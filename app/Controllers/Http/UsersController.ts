a0ce9dde5083cdadf61fd56fd7f3e478101555de
Mateus <mateusgurgel15@gmail.com>
2023-05-11 11:33:40 -0300

    User Created

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,9 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UsersController {
+  public async store({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+
+  public async auth({}: HttpContextContract) {}
+}
