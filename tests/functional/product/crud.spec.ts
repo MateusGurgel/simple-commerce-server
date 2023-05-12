4577f988153959dae9fcc989933d018f3f4ae6fe
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-05-12 02:28:29 -0300

    Product tests created

---
+++ tests/functional/product/crud.spec.ts
@@ -0,0 +1,43 @@
+import { test } from '@japa/runner'
+import User from 'App/Models/User'
+import Product from 'App/Models/Product'
+import getRandomProductData from '../../../utils/getRandomProductData'
+import getRandomAdminData from '../../../utils/getRandomAdminData'
+
+test.group('Product crud', () => {
+  test('Creating product', async ({ client }) => {
+    const user = await User.create(getRandomAdminData())
+    const response = await client.post('/products').form(getRandomProductData()).loginAs(user)
+
+    response.assertStatus(201)
+    response.assertTextIncludes('id')
+  })
+  test('List products', async ({ client }) => {
+    const response = await client.get('/products')
+
+    response.assertStatus(200)
+    response.assertBodyContains('products')
+  })
+
+  test('Update product', async ({ client }) => {
+    const product = await Product.create(getRandomProductData())
+    const user = await User.create(getRandomAdminData())
+
+    const response = await client
+      .patch(`/products/${product.id}`)
+      .form(getRandomAdminData())
+      .loginAs(user)
+
+    response.assertStatus(200)
+    response.assertBodyContains('id')
+  })
+
+  test('Deleting product', async ({ client, assert }) => {
+    const product = await Product.create(getRandomProductData())
+    const user = await User.create(getRandomAdminData())
+    const response = await client.delete(`/products/${product.id}`).loginAs(user)
+
+    assert.isTrue(product.$isDeleted)
+    response.assertStatus(200)
+  })
+})
