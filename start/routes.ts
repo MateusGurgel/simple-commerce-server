da6b0186932f196aa0b2c7e6ace1572920f80b49
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-06-03 19:05:48 -0300

    Auth Middleware added

--- start/routes.ts
+++ start/routes.ts
@@ -21,14 +21,14 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('products', 'ProductsController.index')
-Route.post('products', 'ProductsController.store')
+Route.post('products', 'ProductsController.store').middleware('auth:api')
 Route.get('products/:id', 'ProductsController.show')
-Route.patch('products/:id', 'ProductsController.update')
-Route.delete('products/:id', 'ProductsController.destroy')
+Route.patch('products/:id', 'ProductsController.update').middleware('auth:api')
+Route.delete('products/:id', 'ProductsController.destroy').middleware('auth:api')
 
 Route.post('users', 'UsersController.store')
 Route.post('users/auth', 'UsersController.auth')
-Route.delete('users/:id', 'UsersController.destroy')
+Route.delete('users/:id', 'UsersController.destroy').middleware('auth:api')
 
-Route.get('orders', 'OrdersController.index')
-Route.post('orders', 'OrdersController.store')
+Route.get('orders', 'OrdersController.index').middleware('auth:api')
+Route.post('orders', 'OrdersController.store').middleware('auth:api')
