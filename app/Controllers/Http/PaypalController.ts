89b9eaa80f4cb7192228b012ab81101e3af3bb4c
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-06-17 10:52:20 -0300

    Refactor PaypalController to return appropriate error responses

--- app/Controllers/Http/PaypalController.ts
+++ app/Controllers/Http/PaypalController.ts
@@ -2,6 +2,7 @@ import Env from '@ioc:Adonis/Core/Env'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Order from 'App/Models/Order'
 import paypal from 'App/Services/paypal'
+import ApiResponse from 'App/Utils/ApiResponse'
 import CaptureOrderValidator from 'App/Validators/CaptureOrderValidator'
 
 export default class PaypalsController {
@@ -13,14 +14,18 @@ export default class PaypalsController {
   public async captureOrder({ request, response }: HttpContextContract) {
     const paypalOrder = await request.validate(CaptureOrderValidator)
 
-    const order = await Order.findByOrFail('checkOutOrderId', paypalOrder.id)
+    const order = await Order.findBy('checkOutOrderId', paypalOrder.id)
+
+    if (!order) {
+      return ApiResponse.error(response, 500, [{ message: 'order not found' }])
+    }
 
     try {
       const captureData = await paypal.capturePayment(paypalOrder.id)
       order.isPaid = true
       response.status(200).json(captureData)
     } catch (err) {
-      return response.status(500).send(err.message)
+      return ApiResponse.error(response, 500, err.message)
     }
 
     await order.save()
